51df1439b61eeb3ef54d02ed101502f6f8193671
Pau Fracés <pfraces@gmail.com>
2013-02-15 00:37:18 +0100

    [bugfix] toArray returns [] with undefined

--- index.js
+++ index.js
@@ -67,5 +67,8 @@ var select = function (selector) {
 };
 
 var toArray = function (obj) {
+  if (typeof obj === 'undefined') {
+    return [];
+  }
   return obj.length ? Array.prototype.slice.call(obj) : [].concat(obj);
 };
